dd2f716ed6c59bcc38411a27106f706588a50319
Bill Reiss <bireis@microsoft.com>
2024-02-12 20:42:55 -0500

    Add project files.

---
+++ CollectionViewOverlappingIssue/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CollectionViewOverlappingIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        var dbSource = new ObservableCollection<string>();
+        for (int i = 0; i < 40; i++)
+        {
+            dbSource.Add("Welcome to dot net Multi" + i);
+        }
+        cv.ItemsSource = dbSource;
+    }
+
+    private void populate_Clicked(object sender, EventArgs e)
+    {
+        headLine.Text = "dot net bot in a race car number 8";
+        subHeadLine.Text = "Welcome to dot net Multi platform App";
+    }
+}
+
+
